bfdd3c083f0592cb00856dbcda9a9aa977db3545
c0b41 <cobaimelan@yandex.com>
2023-01-08 23:27:07 +0300

    Move correct directory

---
+++ packages/workerd-service/src/index.ts
@@ -0,0 +1,4 @@
+import { default as kvService } from './lib/plugins/kv/index'
+import { default as cacheService } from './lib/plugins/cache/index'
+
+export { kvService, cacheService }
--- workerd-service/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { default as kvService } from './lib/plugins/kv'
-import { default as cacheService } from './lib/plugins/cache'
-
-export { kvService, cacheService }
